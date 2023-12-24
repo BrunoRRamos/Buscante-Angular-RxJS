01b82400662dff5e68c0e0d2587c1af5aeb4d415
BrunoRRamos <bruno.ramos@ccc.ufcg.edu.br>
2023-12-24 19:22:46 -0300

    feat: Criando acesso a API de consulta

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
-
+export class ListaLivrosComponent implements OnDestroy {
   listaLivros: [];
+  campoBusca: string = '';
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private livroService: LivroService) {}
 
+  buscarLivros() {
+    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
+      next: retornoAPI => this.listaLivros = retornoAPI,
+      error: erro => console.error(erro),
+      complete: () => console.log('Completed')
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
 
 
